3e41ac0b5d20e87b3cad3cfbe6e9029ae9e045e6
sivaadda <sivapriyaadda@gmail.com>
2025-03-28 11:29:06 +0530

    browser router basename change

--- gear-website/src/index.js
+++ gear-website/src/index.js
@@ -11,7 +11,7 @@ import { BrowserRouter } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/Gear01"> {/* Add basename here if needed */}
+    <BrowserRouter basename="/"> {/* Add basename here if needed */}
       <App />
     </BrowserRouter>
   </React.StrictMode>
